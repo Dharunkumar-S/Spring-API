1943e98609fdb67c5f47ab57619be028859c7ec2
Dharunkumar S <dharunkumar1104@gmail.com>
2023-04-27 08:29:36 +0530

    Update studentController.java

--- Day6/src/main/java/com/example/demo/controller/studentController.java
+++ Day6/src/main/java/com/example/demo/controller/studentController.java
@@ -3,8 +3,11 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +30,30 @@ public class studentController {
 	{
 		return studService.saveStudent(s);
 	}
+	
+	@PutMapping(value="/updateStudent/{rno}")
+	public student updateStudent(@RequestBody student s,@PathVariable("rno") int regno)
+	{
+		return studService.updateStudent(s,regno);
+	}
+	
+	@DeleteMapping(value="/deleteStudent/{rno}")
+	public void deleteStudent(@PathVariable("rno") int regno)
+	{
+		studService.deleteStudent(regno);
+	}
+	
+	@GetMapping(value="/getStudent/{rno}")
+	public student getStudent(@PathVariable("rno") int regno)
+	{
+		return studService.getStudent(regno);
+	}
+	
+	 @GetMapping(value="/sortStudent/{field}")
+	 public List<student> sortStudent(@PathVariable("field") String field)
+	 {
+		 return studService.sortStudent(field);
+	 }
+	 
 	 
 }
