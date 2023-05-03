6a098a6ad529cb13c86d1e86426dc8a9e440c972
Dharunkumar S <dharunkumar1104@gmail.com>
2023-05-03 10:40:39 +0530

    spring boot

---
+++ Day7/src/main/java/com/example/demo/controller/PersonController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Person;
+import com.example.demo.service.PersonService;
+
+@RestController
+public class PersonController {
+
+	@Autowired
+	PersonService pService;
+	
+	@GetMapping("/getAllPersons")
+	public List<Person> fetchAllPersons() {
+		List<Person> pList=pService.fetchAllPersons();
+		return pList;
+	}
+}
